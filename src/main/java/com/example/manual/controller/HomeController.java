a8a5c708467ed961fe8d43cf78433ced6708f7df
miki <wakamiki@me.com>
2026-03-29 16:08:07 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/example/manual/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.manual.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Manual Management System is running.";
+    }
+}
